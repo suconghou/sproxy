b9a943bb2e7393be037bc81392ae1232227d9039
suconghou <1126045770@qq.com>
2018-05-05 23:59:21 +0800

    init

--- src/index.js
+++ src/index.js
@@ -12,33 +12,37 @@ const port = process.env.PORT || 80;
 const sslport = process.env.SSLPORT || 443;
 const echoport = process.env.ECHOPORT;
 
-const forward_host = process.env.FORWARD_HOST;
-const forward_port = process.env.FORWARD_PORT;
+const f_host = process.env.FORWARD_HOST;
+const f_port = process.env.FORWARD_PORT;
+
+const elog = e => console.error(e.toString());
 
 if (p(echoport)) {
 	serve(echo).listen(echoport);
-} else if (forward_host && p(forward_port)) {
+} else if (f_host && p(f_port)) {
 	serve(fwd).listen(port);
 } else {
 	const handler = socket => {
-		socket.once("data", data => {
-			let host, port;
-			if (istls(data)) {
-				host = sni(data);
-				port = sslport;
-			} else {
-				const info = gethost(data);
-				host = info.host;
-				port = info.port;
-			}
-			if (host) {
-				forward(socket, data, host, port);
-			} else {
-				socket.write(data, () => {
-					socket.pipe(socket);
-				});
-			}
-		});
+		socket
+			.once("data", data => {
+				let host, port;
+				if (istls(data)) {
+					host = sni(data);
+					port = sslport;
+				} else {
+					const info = gethost(data);
+					host = info.host;
+					port = info.port;
+				}
+				if (host) {
+					forward(socket, data, host, port);
+				} else {
+					socket.write(data, () => {
+						socket.pipe(socket);
+					});
+				}
+			})
+			.on("error", elog);
 	};
 
 	serve(handler).listen(port);
