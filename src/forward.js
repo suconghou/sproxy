1a3f0fc957ea11fe720834dfd8cd9164e1df8274
suconghou <1126045770@qq.com>
2018-05-18 19:17:27 +0800

    update

--- src/forward.js
+++ src/forward.js
@@ -1,24 +1,23 @@
 import net from "net";
+const r = "error";
 export default (socket, data, host, port) => {
-	try {
-		const s = net.createConnection({ host, port });
-		s.write(data, () => {
-			s.pipe(socket);
-			socket.pipe(s);
-		});
-	} catch (e) {
-		elog(e);
-	}
+	const s = net
+		.createConnection({ host, port }, () => {
+			s.write(data, () => {
+				s.pipe(socket);
+				socket.pipe(s);
+			});
+		})
+		.on(r, elog);
 };
 
 export const fwd = (socket, host, port) => {
-	try {
-		const s = net.createConnection({ host, port });
-		s.pipe(socket);
-		socket.pipe(s);
-	} catch (e) {
-		elog(e);
-	}
+	const s = net
+		.createConnection({ host, port }, () => {
+			s.pipe(socket);
+			socket.pipe(s);
+		})
+		.on(r, elog);
 };
 
 export const elog = e => console.error(e.toString());
